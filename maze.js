90131af3c0a853cd083078c0f3332bb805f1e967
fader123 <troysmith620@gmail.com>
2018-10-18 02:19:30 -0500

    exercise 3 completed

--- maze.js
+++ maze.js
@@ -2,7 +2,10 @@ function allevents() {
 
 let first=document.getElementById('boundary1');
 let walls=document.querySelectorAll('#maze > .boundary');
-console.log(walls);
+let num=0;
+
+let end=document.getElementById('end');
+end.addEventListener('mouseover',win);
     
 //first.addEventListener('mouseover',lose);
 for (let x=0;x<walls.length;x++){
@@ -12,6 +15,14 @@ function lose(){
      for (let x=0;x<walls.length;x++){
         walls[x].classList.add('youlose');
      }
+    num=1;
+}
+   
+function win(){
+    //console.log(num);
+    if (num===0){
+        alert('You win!');
+    }
 }
 
 }
